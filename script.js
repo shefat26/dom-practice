05b2f3604c1d6159b0de700fbd5ed97e377db1cb
shefat26 <shefatmoral@gmail.com>
2024-02-14 22:13:30 -0500

    Traversing Dom Element

--- script.js
+++ script.js
@@ -67,8 +67,93 @@ console.log(typeof(document))
 
 //check title
 console.log(document.title);
-console.log()
-console.log()
+
+// check URL
+//console.log(document.URL)
+
+// CHECK DOMAIN
+//console.log(document.domain)
+
+//check header 
+//console.log(document.head)
+
+//check body, not able to see html  body structure
+//console.log(document.body)
+
+
+//console.log(document.all);
+
+
+// find images through dom
+
+//console.log(document.images);
+
+
+// find links through dom
+
+//console.log(document.links);
+
+
+// find forms through dom
+
+//console.log(document.forms);
+
+
+
+
+
+
+
+
+// using for of loop
+
+// for(let element of document.all){
+
+//     console.log(element)
+// }
+
+// changing Title dynamically from script file
+
+document.title = "Play with DOM"
+
+
+
+
+/////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+
+
+
+
+// LESSION #3 - Traversing Dom Elements
+
+
+// Selecting Document Element
+
+console.log(document.getElementById("hero"))
+
+
+
+//// Selecting Document Element and changing the element
+
+
+
+let rocketElement = document.getElementById("rockets")
+
+//rocketElement.textContent = "Acme Rockets Fly"
+
+// we can also use inner text
+rocketElement.innerText = "Acme Rockets Fly"
+
+// if we want to look for text with html, we can't see the html tags here
+console.log(rocketElement.innerHTML) 
+
+
+
+
+
 
 
 
