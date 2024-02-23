04b78e1088340cb68bcb76ebc79b1ba4f5789a2f
shefat26 <shefatmoral@gmail.com>
2024-02-22 20:02:02 -0500

    dom script practice

---
+++ domTwo/script.js
@@ -0,0 +1,35 @@
+
+
+
+//checking document
+console.dir(document)
+
+console.log(typeof(document))
+
+
+//check title
+console.log(document.title);
+
+// check URL
+console.log(document.URL)
+
+// CHECK DOMAIN
+//console.log(document.domain)
+
+//check header 
+console.log(document.head)
+
+//check body, not able to see html  body structure
+console.log(document.body)
+
+
+//console.log(document.all);
+
+
+// find images through dom
+
+console.log(document.images);
+
+
+
+
